c5c09511c0757826c21a7fd815adb9928e8c4528
Rishabh-Saket <87156299+Rishabh-Saket@users.noreply.github.com>
2023-08-26 16:36:13 +0530

    DB creation using sequelize

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express=require('express');
+const bodyParser=require('body-parser');
 const {PORT}=require('./config/serverConfig');
 const setupAndStartServer= async ()=> {
     const app=express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
     app.listen(PORT,()=>{
         console.log(`Server is ready to listen on port ${PORT}`);
     });
