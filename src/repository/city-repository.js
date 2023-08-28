3b22fefc88deeecaf91f70091b0214da1434835c
Rishabh-Saket <87156299+Rishabh-Saket@users.noreply.github.com>
2023-08-28 16:03:08 +0530

    Compelted city repository

---
+++ src/repository/city-repository.js
@@ -0,0 +1,56 @@
+const {City}=require('../models/index');
+
+class CityRepository{
+    async creatCity({name}){
+        try {
+            const city=await City.create({
+                name
+            });
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the respository layer");
+            throw {error};
+        }
+    }
+
+    async deleteCity(cityId){
+        try {
+            await City.destroy({
+                where:{
+                    id:cityId
+                }
+            });
+            return true;
+        } catch (error) {
+            console.log("Something went wrong in the respository layer");
+            throw {error};
+        }
+    }
+
+    async updateCity(cityId,data){
+        try {
+            const city=await City.update(data,{
+                where:{
+                    id:cityId
+                }
+            });
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the respository layer");
+            throw {error};
+        }
+    }
+
+    async getCity(cityId){
+        try {
+            const city=await City.findByPK(cityId);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the respository layer");
+            throw {error};
+        }
+    }
+
+}
+
+module.exports=CityRepository;
