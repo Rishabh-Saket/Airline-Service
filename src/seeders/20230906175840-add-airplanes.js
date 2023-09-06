d6fbe228305ee3dc931fbfd105a700071ea08acf
Rishabh-Saket <87156299+Rishabh-Saket@users.noreply.github.com>
2023-09-06 23:47:57 +0530

    Added airplanes

---
+++ src/seeders/20230906175840-add-airplanes.js
@@ -0,0 +1,58 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+   await queryInterface.bulkInsert('Airplanes',[
+    {
+      modelNumber: 'Airbus A380-800',
+      capacity: 835,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      modelNumber: 'Boeing 747-8',
+      capacity: 426,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      modelNumber: 'Boeing 747-400',
+      capacity: 416,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      modelNumber: 'Airbus A350-1000',
+      capacity: 350,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      modelNumber: 'Boeing 787-10 Dreamliner',
+      capacity: 330,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+   ],{});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
